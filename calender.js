ef9ed1e8a2998fb00908906402459d58dbe2cb36
Amy Lewis <amy.555.lewis@gmail.com>
2024-04-24 16:41:25 +0100

    merge

--- calender.js
+++ calender.js
@@ -1,32 +1,3 @@
-// Compulsory user inputs: name, period start date
-
-// If period length undefined, default to 5 days
-// If cycle length undefined, default to 28 days
-
-// Create object to store user data
-const userData = {
-  name: "",
-  periodStartDate: "",
-  periodLength: "",
-  cycleLength: "",
-};
-
-// Serialize object to put into localStorage
-const userDataSerialized = JSON.stringify(userData);
-// Store in localStorage
-localStorage.setItem("userData", userDataSerialized);
-// Retrieve from localStorage
-const userName = JSON.parse(localStorage.getItem("userData")).name;
-const userPeriodStartDate = JSON.parse(
-  localStorage.getItem("userData")
-).periodStartDate;
-const userPeriodLength = JSON.parse(
-  localStorage.getItem("userData")
-).periodLength;
-const userCycleLength = JSON.parse(
-  localStorage.getItem("userData")
-).cycleLength;
-
 //Variables
 let ButtonToYear = document.getElementById('change-year')
 let ButtonToMonth = document.getElementById('change-month')
@@ -105,4 +76,3 @@ function GetValues() {
       });
       calendar.render();
 };*/
-
