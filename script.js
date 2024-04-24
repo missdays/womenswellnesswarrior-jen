5dd55405722573243c060717219ab78de64d72b1
Dan Morriss <danielmorriss1@gmail.com>
2024-04-24 12:51:46 +0100

    Add javascript to access and retrieve from localStorage

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-//user input
-//boolean are you on your period currently?
-// when did your  period/ last period start
-// how long is a typical period (if idk selected choose 5 days)
-// calendar insert for days 26-28 for reduced activity
-// day 1/2 recommend higher absorbency products
+// Compulsory user inputs: name, period start date
+// If period length undefined, default to 5 days
+// If cycle length undefined, default to 28 days
+
+// Create object to store user data
+const userData = {
+  name: "",
+  periodStartDate: "",
+  periodLength: "",
+  cycleLength: "",
+};
+
+// Serialize object to put into localStorage
+const userDataSerialized = JSON.stringify(userData);
+// Store in localStorage
+localStorage.setItem("userData", userDataSerialized);
+// Retrieve from localStorage
+const userName = JSON.parse(localStorage.getItem("userData")).name;
+const userPeriodStartDate = JSON.parse(
+  localStorage.getItem("userData")
+).periodStartDate;
+const userPeriodLength = JSON.parse(
+  localStorage.getItem("userData")
+).periodLength;
+const userCycleLength = JSON.parse(
+  localStorage.getItem("userData")
+).cycleLength;
